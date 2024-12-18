d845df4ece5971dfee764ec6044b8e1637a24485
Manish85111 <manishsingh851111@gmail.com>
2024-12-18 19:24:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
+import icons from "./icons";
+import images from "./images";
+import theme, { COLORS, SIZES, FONTS } from "./theme";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export { icons, images, theme, COLORS, SIZES, FONTS };
